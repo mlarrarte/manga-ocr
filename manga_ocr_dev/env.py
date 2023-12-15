f9eb026447d5ea7d0b5d0e27b6149dea04345bc7
Mikel Larrarte <110637385+mlarrarte@users.noreply.github.com>
2023-12-15 23:07:16 +0100

    Update env.py

--- manga_ocr_dev/env.py
+++ manga_ocr_dev/env.py
@@ -4,6 +4,6 @@ ASSETS_PATH = Path(__file__).parent.parent / 'assets'
 
 FONTS_ROOT = Path('~/data/jp_fonts').expanduser()
 DATA_SYNTHETIC_ROOT = Path('~/data/manga/synthetic').expanduser()
-BACKGROUND_DIR = Path('~/data/manga/Manga109s/background').expanduser()
-MANGA109_ROOT = Path('~/data/manga/Manga109s').expanduser()
+BACKGROUND_DIR = Path('~/data/manga/Manga109/background').expanduser()
+MANGA109_ROOT = Path('~/data/manga/').expanduser()
 TRAIN_ROOT = Path('~/data/manga/out').expanduser()
