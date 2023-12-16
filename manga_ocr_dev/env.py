12114d360e241418578660d6cc2178417ca4e854
Mikel Larrarte <110637385+mlarrarte@users.noreply.github.com>
2023-12-16 10:30:04 +0100

    Update env.py

--- manga_ocr_dev/env.py
+++ manga_ocr_dev/env.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 ASSETS_PATH = Path(__file__).parent.parent / 'assets'
 
-FONTS_ROOT = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/jp_fonts').expanduser()
-DATA_SYNTHETIC_ROOT = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/manga/synthetic').expanduser()
-BACKGROUND_DIR = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/manga/Manga109/background').expanduser()
-MANGA109_ROOT = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/manga/Manga109').expanduser()
-TRAIN_ROOT = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/manga/out').expanduser()
+FONTS_ROOT = Path('/content/drive/MyDrive/PROIEKTUA/data/jp_fonts').expanduser()
+DATA_SYNTHETIC_ROOT = Path('/content/drive/MyDrive/PROIEKTUA/data/manga/synthetic').expanduser()
+BACKGROUND_DIR = Path('/content/drive/MyDrive/PROIEKTUA/data/manga/Manga109/background').expanduser()
+MANGA109_ROOT = Path('/content/drive/MyDrive/PROIEKTUA/data/manga/Manga109').expanduser()
+TRAIN_ROOT = Path('/content/drive/MyDrive/PROIEKTUA/data/manga/out').expanduser()
