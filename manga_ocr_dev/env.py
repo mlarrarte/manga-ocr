dc095394d959c5365ff8b870cec2df94b894b207
Mikel Larrarte <110637385+mlarrarte@users.noreply.github.com>
2023-12-16 22:07:26 +0100

    Update env.py

--- manga_ocr_dev/env.py
+++ manga_ocr_dev/env.py
@@ -4,6 +4,6 @@ ASSETS_PATH = Path(__file__).parent.parent / 'assets'
 
 FONTS_ROOT = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/jp_fonts').expanduser()
 DATA_SYNTHETIC_ROOT = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/manga/synthetic').expanduser()
-BACKGROUND_DIR = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/manga/Manga109_released_2021_12_30/background').expanduser()
-MANGA109_ROOT = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/manga/Manga109_released_2021_12_30').expanduser()
+BACKGROUND_DIR = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/manga/Manga109/background').expanduser()
+MANGA109_ROOT = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/manga/Manga109').expanduser()
 TRAIN_ROOT = Path('/content/drive/MyDrive/URTEA3/HP/PROIEKTUA/data/manga/out').expanduser()
